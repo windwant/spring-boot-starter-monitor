0e816b9c63ca9306bb8bd6ebf39f3813f34dcce1
windwant <nie.junlei@hotmail.com>
2018-11-23 11:35:44 +0800

    调整文件名，添加注解，README说明

--- src/main/java/org/windwant/monitor/starter/config/MonitorAutoConfigPointcutAdvisor.java
+++ src/main/java/org/windwant/monitor/starter/config/MonitorAutoConfigPointcutAdvisor.java
@@ -8,27 +8,29 @@ import org.springframework.aop.support.AbstractPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.context.annotation.Configuration;
 import org.windwant.monitor.starter.annotation.Monitor;
-import org.windwant.monitor.starter.interceptor.MonitorInterceptor;
+import org.windwant.monitor.starter.interceptor.MonitorAdvice;
 
 import javax.annotation.PostConstruct;
 
 /**
+ * 编织器 关联PointCut和Advice
+ * 一般继承AbstractPointcutAdvisor类，需要子类提供 Pointcut 和 Advice 实例
  * Created by Administrator on 18-5-2.
  */
 @Configuration
-public class MonitorAutoConfig extends AbstractPointcutAdvisor {
+public class MonitorAutoConfigPointcutAdvisor extends AbstractPointcutAdvisor {
 
-    private static final Logger logger = LoggerFactory.getLogger(MonitorAutoConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(MonitorAutoConfigPointcutAdvisor.class);
 
-    private Pointcut pointcut;
+    private Pointcut pointcut;//在什么地方执行
 
-    private Advice advice;
+    private Advice advice; //执行什么；BeforeAdvice，AfterAdvice，Interceptor，一般实现MethodInterceptor接口
 
     @PostConstruct
     public void init() {
         logger.info("monitor config start");
-        this.pointcut = new AnnotationMatchingPointcut(null, Monitor.class);
-        this.advice = new MonitorInterceptor();
+        this.pointcut = new AnnotationMatchingPointcut(null, Monitor.class);//创建Monitor声明PointCut；织入点
+        this.advice = new MonitorAdvice();
         logger.info("monitor config end");
     }
 
--- src/main/java/org/windwant/monitor/starter/interceptor/MonitorAdvice.java
+++ src/main/java/org/windwant/monitor/starter/interceptor/MonitorAdvice.java
@@ -5,16 +5,20 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 /**
+ * ∂®“ÂAdvice
  * Created by Administrator on 18-5-2.
  */
-public class MonitorInterceptor implements MethodInterceptor {
-    private static final Logger logger = LoggerFactory.getLogger(MonitorInterceptor.class);
+public class MonitorAdvice implements MethodInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(MonitorAdvice.class);
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
         long start = System.currentTimeMillis();
         Object result = methodInvocation.proceed();
         long end = System.currentTimeMillis();
-        logger.info("method: {} execute : {} milliseconds", methodInvocation.getMethod().getName(), (end - start));
+        logger.info("method: {}, args: {}, execute : {} milliseconds",
+                methodInvocation.getMethod().getName(), Arrays.toString(methodInvocation.getArguments()), (end - start));
         return result;
     }
 }
